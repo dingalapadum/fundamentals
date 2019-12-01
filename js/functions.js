ebe094ff0a82d56852c50708b10201936c7682b8
dinga <dingalapadum@gmail.com>
2019-12-01 01:52:08 +0100

    js: make formatting of different files consistent

--- js/functions.js
+++ js/functions.js
@@ -40,7 +40,9 @@ function* indexGenerator() {
 }
 
 
-/**
+
+
+/** ----------------------------------------------------------------------------
  * 1. Function declartion
  * ----------------------
  * - starts with _function_ keyword
@@ -48,7 +50,7 @@ function* indexGenerator() {
  * - a list of parameters in (arg1, arg2, ..., argN)
  * - Body in curly braces
  */
-
+section("Function declartion");
 // is hoisted, i.e. it can be used before declaration
 console.log(hello('hoisting'));
 
@@ -75,15 +77,19 @@ function nested() {
 // nested().inner(); //TypeError: Cannot read property 'inner' of undefined
 nested();
 
+console.log();
 
-/**
+
+
+
+/** ----------------------------------------------------------------------------
  * 2. Function expressions
  * -----------------------
  * (everything that is not a function declaration is a function expression. 
  * I.e. if it does not start with the 'function' keyword) but usually it
  * is assigned to a variable
  */
-
+section("Function expressions");
 count = function (array) {
     return array.length;
 }
@@ -100,12 +106,17 @@ foo = function bar(arg) {
 console.log(foo.name);
 console.log(foo('man'));
 
-/**
+console.log();
+
+
+
+
+/** ----------------------------------------------------------------------------
  * 3. Shorthand method definition
  * ------------------------------
  * ... in object literals
  */
-
+section("Shorthand method definition");
 myObject = {
     a: 'someprop',
     sayHi() {
@@ -130,28 +141,37 @@ const collection = {
 };
 collection[addMethod]('C', 'Java', 'PHP');
 console.log(collection[getMethod](1)); // => 'Java'
+console.log();
 
 
-/**
+
+
+/** ----------------------------------------------------------------------------
  * 4. Arrow function
  * -----------------
  * General form
  * (param1, param2, ..., paramN) => {...}
  */
-
+section("Arrow function");
 helloArrow = (arg) => {
     let msg = 'Arrow says: ' + arg;
     return msg;
 }
 
 console.log(helloArrow('hello!'));
+console.log();
 
-/**
+
+
+
+/** ----------------------------------------------------------------------------
  * 5. Generator function
  * ---------------------
+ * - Generator functions use 'yield' instead of 'return'
+ * - You get there 'yield' value by calling <functionName>.next().value
  * the example is a function declaration, but this also works for function expressions
  */
-
+section("Generator function");
 // note the * and note it is 'yield' instead of return
 function* indexGenerator() {
     var index = 0;
@@ -164,13 +184,25 @@ const g = indexGenerator();
 console.log(g.next().value); // => 0
 console.log(g.next().value); // => 1
 
-/**
+
+
+
+/** ----------------------------------------------------------------------------
  * 6. new Function
  * ---------------
  * not very relevant.
+ * Think of it as creating 'as a runtime function instance'
  * Remember that functions almost never should be declared using 
  * new Function(). Because the function body is evaluated on runtime, 
  * this approach inherits many eval() usage problems: security risks, 
  * harder debugging, no way to apply engine optimizations, 
  * no editor auto-complete.
  */
+
+
+ /** ====================== utily ====================== */
+function section(title) {
+    console.log("----------------------------------------------------------------");
+    console.log(title);
+    console.log("----------------------------------------------------------------");
+}
--- js/template_literals.js
+++ js/template_literals.js
@@ -12,7 +12,6 @@
  * Syntax
  * ------
  */
-
 let tl_a = `string text`
 
 let tl_b = `string text line 1
@@ -22,10 +21,13 @@ let expression = 5 + 7;
 let tl_c = `string text ${expression} string text`
 
 
-/**
+
+
+/**----------------------------------------------------------------------------
  * Multi-line strings
  * ------------------
  */
+section("Multi-line strings");
 // normal multi-line string printing
 console.log('string text line 1\n' +
     'string text line 2');
@@ -41,11 +43,16 @@ let a = 5;
 let b = 10;
 console.log(`Fifteen is ${a + b} and
 not ${2 * a + b}.`);
+console.log();
 
-/**
+
+
+/**----------------------------------------------------------------------------
  * Nesting
  * ------- 
  * */
+section("Nesting");
+console.log("on output here - this is only demonstarted directly in the code");
 
 // ES5
 let truth = true;
@@ -61,24 +68,41 @@ const classes2 = `header ${truth ? '' :
 const classes3 = `header ${truth ? '' :
     `icon-${item.isCollapsed ? 'expander' : 'collapser'}`}`;
 
-/**
+    console.log();
+
+
+
+/**----------------------------------------------------------------------------
  * Tagged templates
  * ----------------
  */
- function myFun(arg){
+section("Tagged templates");
+function myFun(arg) {
     return `Hi ${arg}`;
- }
+}
 
- console.log(myFun('you!'));
+console.log(myFun('you!'));
 
- /**
+
+
+
+/**----------------------------------------------------------------------------
   * Raw strings
   * -----------
   * - 'return the evaluated template literal'
   * 
   * note that it still would evaluate the ${...} parts
   */
- let tl = `Hello \n world! ${5+2}`;
-let str = String.raw`Hello \n world ${5+2}`;
+section("Raw strings");
+let tl = `Hello \n world! ${5 + 2}`;
+let str = String.raw`Hello \n world ${5 + 2}`;
 console.log(tl);
 console.log(str);
+
+
+/** ====================== utily for this file ====================== */
+function section(title) {
+    console.log("----------------------------------------------------------------");
+    console.log(title);
+    console.log("----------------------------------------------------------------");
+}
