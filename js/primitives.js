8d92411df9d3ab99ba43b1d6025ef8594673d6b7
dinga <dingalapadum@gmail.com>
2019-12-03 22:14:27 +0100

    js: start with primitives

---
+++ js/primitives.js
@@ -0,0 +1,22 @@
+/**
+ * Primitive Datatypes
+ * ===========================
+ * 
+ * These examples are inspired or come from: 
+ * https://dmitripavlutin.com/6-ways-to-declare-javascript-functions/
+ * 
+ * run this file using 'node primitives.js'
+ * 
+ ####################################################################### */
+/**
+These six types are considered to be primitives:
+
+    Boolean — true or false
+    Null — no value
+    Undefined — a declared variable but hasn’t been given a value
+    Number — integers, floats, etc
+    String — an array of characters i.e words
+    Symbol — a unique value that's not equal to any other value
+
+A primitive is not an object. All primitives are immutable.
+*/
