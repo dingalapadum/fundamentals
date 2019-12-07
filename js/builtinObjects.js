77b94483137a709c93bbfd3fc2df36cae50ae95e
dinga <dingalapadum@gmail.com>
2019-12-07 23:59:10 +0100

    js: start with builtinObjects

---
+++ js/builtinObjects.js
@@ -0,0 +1,16 @@
+/**
+ * Built in Objects
+ * ================
+ * 
+ * These examples are inspired or come from: 
+ * 
+ * run this file using 'node builtinObjects.js'
+ * 
+ ####################################################################### */
+/**
+ * Basic Objects
+ * -------------
+ * - Object
+ * - Array
+ * 
+ */
